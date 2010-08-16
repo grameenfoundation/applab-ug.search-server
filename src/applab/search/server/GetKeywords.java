af91d064d779b5c9f10986f7f13f83ede6bdb2b5
kwolf <kwolf@8df75f15-59d7-5c4e-8359-c3bff8332439>
2010-08-16 11:12:18 +0000

    Scaffolding for Search Server
    git-svn-id: http://pmo.applab.org:81/svn/ckw/products/search.server@288 8df75f15-59d7-5c4e-8359-c3bff8332439

---
+++ src/applab/search/server/GetKeywords.java
@@ -0,0 +1,41 @@
+package applab.search.server;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import applab.server.ApplabServlet;
+
+/**
+ * Server method that returns the keywords requested by the client.
+ * 
+ * Implements the partial keywords algorithm described in the CKW Search Server functional specification. 
+ *
+ */
+public class GetKeywords extends ApplabServlet {
+    private static final long serialVersionUID = 1L;
+
+    // given a post body like:
+    // <?xml version="1.0"?>
+    // <GetKeywordsRequest xmlns="http://schemas.applab.org/2010/07/seach">
+    //   <localKeywordsVersion>Mon, 12 Jul 2010 18:08:33 +0000</localKeywordsVersion>
+    // </GetKeywordsRequest>
+    // 
+    // returns a response like:
+    // <?xml version="1.0"?>
+    // <GetKeywordsResponse xmlns="http://schemas.applab.org/2010/07/search">
+    //  <version>Wed, 21 Jul 2010 11:26:13 +0000</version>
+    //  <add id="23219">Farm_Inputs Sironko Sisiyi Seeds</add>
+    //  <add id="39243">Animals Bees Pests Wax_moths<add/>
+    //  <remove id="45" />
+    // </GetKeywordsResponse>
+    @Override
+    protected void doApplabPost(HttpServletRequest request, HttpServletResponse response) throws IOException, SAXException, ParserConfigurationException, ClassNotFoundException, SQLException {
+        // TODO: add implementation (see applab.surveys.server.Select.java for inspiration) 
+    }
+}
