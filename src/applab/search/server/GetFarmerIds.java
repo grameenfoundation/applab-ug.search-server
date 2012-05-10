1225518daef61f2e2d5c37671c969d6ab24a26d9
James O. Onyango <jonyango@applab.org>
2012-05-10 06:38:40 +0300

    Redone the GetFarmerIds servlet

--- src/applab/search/server/GetFarmerIds.java
+++ src/applab/search/server/GetFarmerIds.java
@@ -1,14 +1,11 @@
 package applab.search.server;
 
-import applab.server.ApplabConfiguration;
 import applab.server.ApplabServlet;
 import applab.server.ServletRequestContext;
-import applab.server.WebAppId;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 import java.util.Random;
 
 import javax.servlet.ServletException;
@@ -21,17 +18,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import com.sforce.soap.enterprise.LoginResult;
-import com.sforce.soap.enterprise.SessionHeader;
-import com.sforce.soap.enterprise.SforceServiceLocator;
-import com.sforce.soap.enterprise.SoapBindingStub;
-import com.sforce.soap.enterprise.fault.InvalidIdFault;
-import com.sforce.soap.enterprise.fault.LoginFault;
-import com.sforce.soap.enterprise.fault.UnexpectedErrorFault;
-import com.sforce.soap.schemas._class.PreRegisterFarmers.BulkRegisterFarmers;
-import com.sforce.soap.schemas._class.PreRegisterFarmers.PreRegisterFarmersBindingStub;
-import com.sforce.soap.schemas._class.PreRegisterFarmers.PreRegisterFarmersServiceLocator;
-
 /**
  * Servlet implementation class GetFarmerIds
  */
@@ -42,7 +28,7 @@ public class GetFarmerIds extends ApplabServlet {
     public static final int FARMER_ID_SET_SIZE = 100;
 
     /**
-     * Default constructor.
+     * Default constructor. 
      */
     public GetFarmerIds() {
         // TODO Auto-generated constructor stub
@@ -50,9 +36,8 @@ public class GetFarmerIds extends ApplabServlet {
 
     @Override
     protected void doApplabGet(HttpServletRequest request, HttpServletResponse response, ServletRequestContext context)
-            throws ServletException, IOException, ServiceException {
-
-        log("Reached get method for Get Farmer Ids");
+            throws Exception {
+        log("Reached get method for get country code");
         doApplabPost(request, response, context);
     }
 
@@ -87,137 +72,15 @@ public class GetFarmerIds extends ApplabServlet {
             e.printStackTrace();
         }
     }
-
-    /**
-     * Stub for Saleforce method
-     * 
-     * @param imei
-     * @param currentFarmerIdCount
-     * @return
-     * @throws RemoteException 
-     * @throws ServiceException 
-     */
+    
     private String getFarmerIdsFromSalesforce(String imei, int currentFarmerIdCount) throws RemoteException, ServiceException {
-        /*
-         * Random rand = new Random();
+       
+        Random rand = new Random();
         long randomValue1 = Math.round(rand.nextDouble() * 10000);
         long randomValue2 = Math.round(rand.nextDouble() * 10000);
-        return String.format("{\"FarmerIds\" : [ {\"Id\" : \"223AB%d\", \"FId\" : \"DF%d\"}, {\"Id\" : \"234CD%d\", \"FId\" : \"CQ%d\"}]}",
-                randomValue1, randomValue1, randomValue2, randomValue2); 
-        */             
-                
-        if (currentFarmerIdCount > 10) {
-            return "";            
-        }        
-        int newIdCount = FARMER_ID_SET_SIZE - currentFarmerIdCount;       
+        return String.format("{\"FarmerIds\" : [ {\"fId\" : \"DF%d\"}, {\"FId\" : \"CQ%d\"}]}",
+                randomValue1, randomValue1, randomValue2, randomValue2);                   
         
-        //TODO 1. First generate New Codes
-        String generatedFarmerIds = generateNewFarmerIds(newIdCount);
-        
-        //3. Push the IDs to Salesforce as comma seperated
-        BulkRegisterFarmers bulkRegisterFarmers = new BulkRegisterFarmers();
-        bulkRegisterFarmers.setImei(imei);
-        bulkRegisterFarmers.setNewFarmerIds(generatedFarmerIds);
-        
-        PreRegisterFarmersBindingStub serviceStub = setupSalesforceAuthentication();
-        bulkRegisterFarmers = serviceStub.preRegisterFarmers(bulkRegisterFarmers);
-        
-        //TODO 4. Save the newIds accepted by Salesforce to the DB and Generate JSON
-        String savedFarmerIds = bulkRegisterFarmers.getSavedIds();
-        
-        String farmerIdsJson = saveNewFarmerIdsAndCreateJson(savedFarmerIds);
-                
-        return farmerIdsJson;
     }
 
-    private String generateNewFarmerIds(int newIdCount) {
-        ArrayList<String> farmerIds = new ArrayList<String>();
-        Random rand = new Random();
-        
-        for (int i=0; i<newIdCount; i++) {
-            //TODO May be get the Max value in DB and have a sequential Number
-            
-            long randomValue = Math.round(rand.nextDouble() * 10000);
-            
-            //Assumed country is UG for now
-            String farmerId = String.format("UG%05d", randomValue);
-            if (farmerIds.contains(farmerId)) {
-                continue;
-            }
-            
-            //TODO Also Check in DB            
-            farmerIds.add(farmerId);
-        }
-        
-        return convertArrayListToCsvString(farmerIds);
-    }
-
-    private PreRegisterFarmersBindingStub setupSalesforceAuthentication() throws ServiceException, RemoteException, InvalidIdFault,
-            UnexpectedErrorFault, LoginFault {
-
-        PreRegisterFarmersServiceLocator preRegisterFarmerServiceLocator = new PreRegisterFarmersServiceLocator();
-        PreRegisterFarmersBindingStub serviceStub = (PreRegisterFarmersBindingStub)preRegisterFarmerServiceLocator.getPreRegisterFarmers();
-
-        // Use soap api to login and get session info
-        SforceServiceLocator soapServiceLocator = new SforceServiceLocator();
-        soapServiceLocator.setSoapEndpointAddress((String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforceAddress", ""));
-        SoapBindingStub binding = (SoapBindingStub)soapServiceLocator.getSoap();
-        LoginResult loginResult = binding.login((String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforceUsername", ""),
-                (String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforcePassword", "")
-                        + (String)ApplabConfiguration.getConfigParameter(WebAppId.global, "salesforceToken", ""));
-        SessionHeader sessionHeader = new SessionHeader(loginResult.getSessionId());
-
-        // Share the session info with our webservice
-        serviceStub.setHeader("http://soap.sforce.com/schemas/class/PreRegisterFarmers ", "SessionHeader", sessionHeader);
-        return serviceStub;
-    }
-    
-    private String saveNewFarmerIdsAndCreateJson(String savedFarmerIds) {
-        
-        StringBuffer sbFarmerIdsJson = new StringBuffer();
-        sbFarmerIdsJson.append("{\"FarmerIds\" : [ ");
-        
-        if(null == savedFarmerIds || savedFarmerIds.isEmpty()) {
-            //TODO Return an empty JSON String
-            sbFarmerIdsJson.append("]}");
-            return sbFarmerIdsJson.toString();
-        }
-        
-        String[] savedFarmerIdsArray = savedFarmerIds.split(",");
-        for (int i=0; i < savedFarmerIdsArray.length; i++) {
-            String savedFarmerId = savedFarmerIdsArray[i];
-            //TODO Save to the Database
-            
-            
-            //Append to the JSON String
-            String fIdJsonPart = "";
-            if(i < savedFarmerIdsArray.length - 1) {
-                fIdJsonPart = String.format("{\"fId\":\"%s\"},",savedFarmerId);
-            }
-            else {
-                fIdJsonPart = String.format("{\"fId\":\"%s\"}]}",savedFarmerId);
-            }
-            
-            sbFarmerIdsJson.append(fIdJsonPart);
-        }        
-        return sbFarmerIdsJson.toString();
-    }
-    
-    private String convertArrayListToCsvString(ArrayList<String> stringArray) {
-        StringBuffer sbFinalString = new StringBuffer();
-        
-        if(null == stringArray || stringArray.isEmpty()) {
-            return sbFinalString.toString();
-        }
-        
-        for (int i=0; i < stringArray.size(); i++) {
-            if (i < stringArray.size() - 1) {
-                sbFinalString.append(stringArray.get(i) + ",");
-            }
-            else {
-                sbFinalString.append(stringArray.get(i));
-            }
-        }        
-        return sbFinalString.toString();
-    }
 }
